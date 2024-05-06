d6429d4a62bff08ae73be66f31653f744d17790c
narawit-tub <narawit.tub@outlook.co.th>
2024-05-06 16:04:55 +0700

    chore: sorting css class according to prettier, action will do on save

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   semi: false,
-  trailingComma: 'all',
+  trailingComma: "all",
   singleQuote: true,
   printWidth: 120,
   tabWidth: 2,
-  endOfLine: 'auto',
-}
+  endOfLine: "auto",
+  plugins: ["prettier-plugin-tailwindcss"]
+};
